48bc3762222dbacec4e201fd4d362070bb2b13f3
sedx876 <swatkins876@gmail.com>
2020-08-30 12:37:28 -0400

    state and prop practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core'
 
 function App() {
+  const [todos, setTodos] = useState(['Read', 'Write', 'Code']);
+  const [input, setInput] = useState('');
+  console.log(input)
+
+  const addTodo = (event) => {
+    event.preventDefault()
+    console.log('It does work!')
+    setTodos([...todos, input])
+    setInput('')
+  }
+
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
+      <FormControl>
+        <InputLabel>Write a ToDo</InputLabel>
+        <Input 
+          value={input} 
+          onChange={event => setInput(event.target.value)} 
+        />
+        <Button disabled={!input} 
+          type="submit" 
+          onClick={addTodo} 
+          variant="contained" 
+          color="primary">
+          Add ToDo
+        </Button>
+      </FormControl>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
